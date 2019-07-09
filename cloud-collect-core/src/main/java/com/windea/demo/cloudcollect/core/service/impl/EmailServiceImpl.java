ebe0385e2da32d509ae13ab279928e49c81bb670
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-10 01:58:13 +0800

    0.1.24 实现导入导出功能，包括控制器。

--- cloud-collect-core/src/main/java/com/windea/demo/cloudcollect/core/service/impl/EmailServiceImpl.java
+++ cloud-collect-core/src/main/java/com/windea/demo/cloudcollect/core/service/impl/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.windea.demo.cloudcollect.core.service.impl;
 
+import com.windea.commons.kotlin.annotation.NotTested;
 import com.windea.demo.cloudcollect.core.service.EmailService;
 import org.springframework.stereotype.Service;
 
 @Service
+@NotTested("未实现……")
 public class EmailServiceImpl implements EmailService {
 	@Override
 	public void sendEmail() {
--- cloud-collect-core/src/main/java/com/windea/demo/cloudcollect/core/service/impl/QiniuServiceImpl.java
+++ cloud-collect-core/src/main/java/com/windea/demo/cloudcollect/core/service/impl/QiniuServiceImpl.java
@@ -1,9 +1,11 @@
 package com.windea.demo.cloudcollect.core.service.impl;
 
+import com.windea.commons.kotlin.annotation.NotTested;
 import com.windea.demo.cloudcollect.core.service.QiniuService;
 import org.springframework.stereotype.Service;
 
 @Service
+@NotTested("未实现……")
 public class QiniuServiceImpl implements QiniuService {
 	@Override
 	public void upload() {
