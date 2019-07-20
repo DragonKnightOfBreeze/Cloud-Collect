83620d8a3b6f78313f33c5221c273fa0ff167824
DragonKnightOfBreeze <2944262241@qq.com>
2019-07-21 00:36:21 +0800

    0.2.2 初始化前端目录。

--- cloud-collect-web/src/views/Home.vue
+++ cloud-collect-web/src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue';
 
-  export default Vue.extend({
+  export default {
     name: 'home',
     components: {
-      HelloWorld,
+      HelloWorld
     },
     methods: {}
-  });
+  }
 </script>
