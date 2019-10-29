13291e88807652fe24485f66006e3800b318e0b2
DragonKnightOfBreeze <2944262241@qq.com>
2019-10-29 14:41:36 +0800

    当为错误页和成功页时隐藏首页头部和尾部。

--- cloud-collect-web/src/App.vue
+++ cloud-collect-web/src/App.vue
@@ -2,18 +2,15 @@
   <div id="app">
     <!--NOTE 不指定direction的话，就会出现奇怪的问题-->
     <ElContainer direction="vertical">
-      <ElHeader>
-        <!--Header-->
+      <ElHeader v-show="showHeader">
         <TheHeader></TheHeader>
       </ElHeader>
 
       <ElMain>
-        <!--Main-->
         <router-view></router-view>
       </ElMain>
 
-      <ElFooter>
-        <!--Footer-->
+      <ElFooter v-show="showFooter">
         <TheFooter></TheFooter>
       </ElFooter>
     </ElContainer>
@@ -23,12 +20,32 @@
 <script lang="ts">
   import TheFooter from "@/components/TheFooter.vue"
   import TheHeader from "@/components/TheHeader.vue"
-  import {Component, Vue} from "vue-property-decorator"
+  import {Component, Vue, Watch} from "vue-property-decorator"
+  import {Route} from "vue-router"
 
   @Component({
     components: {TheHeader, TheFooter}
   })
   export default class App extends Vue {
+    private showHeader = true
+    private showFooter = true
+
+    mounted() {
+      this.routeChange(this.$route, this.$route)
+    }
+
+    //当为错误页和成功页时隐藏首页头部和尾部
+    @Watch("$route")
+    private routeChange(value: Route, oldValue: Route) {
+      if (value.path.startsWith("/error") || value.path.startsWith("/success")) {
+        console.log("隐藏页面首部和尾部。")
+        this.showHeader = false
+        this.showFooter = false
+      } else {
+        this.showHeader = true
+        this.showFooter = true
+      }
+    }
   }
 </script>
 
