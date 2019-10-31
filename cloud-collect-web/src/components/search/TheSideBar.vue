92e59060cd0e487f4c29b043ef2cd0f3f384f074
DragonKnightOfBreeze <2944262241@qq.com>
2019-11-01 02:02:47 +0800

    更新前端页面和代码中。

---
+++ cloud-collect-web/src/components/search/TheSideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--TODO-->
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator"
+
+  @Component
+  export default class TheSideBar extends Vue {
+  }
+</script>
+
+<style scoped>
+
+</style>
